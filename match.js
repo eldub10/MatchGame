17e80eb66ad937c0b18a5e1de9d593caba153624
Larry Wright <eldub10@users.noreply.github.com>
2015-07-23 18:38:59 -0700

    added comments

--- match.js
+++ match.js
@@ -1,3 +1,5 @@
+(function(global) {
+'use strict';
 
 function Match(cards) {
 	this.cards = cards;
@@ -14,107 +16,141 @@ function Match(cards) {
 	this.create();
 }
 
-Match.prototype = {
-	create: function(){
-		this.titleElement.innerText = 'Authors';
-		var html = [];
-		this.cards.forEach(function(card, i) {
-			html[i]  = '<div class="card" data-index="' + i + '">'
-				+ '<div class="front"></div>'
-				+ '<div class="back">'
-				+ '<p class="cardname">' + card.name + '</p>'
-				+ '<img src="' + card.src + '">';
-			for (var n = 0; n < card.details.length; n++) {
-				html[i] += '<p>' + card.details[n] + '</p>';
-			}
-			html[i] += '</div></div>';
-		});
-		html = html.concat(html);
-		html.sort(this.shuffle);
-		this.cardsElement.innerHTML = html.join('');
-
-		var elements = document.getElementsByClassName('front');
-		for(var i = 0; i < elements.length; i++) {
-			elements[i].addEventListener('click', this.cardClicked.bind(this));
+/**
+ * Create cards and render them to html
+ */
+Match.prototype.create = function() {
+	var html = [];
+	this.cards.forEach(function(card, i) {
+		html[i]  = '<div class="card" data-index="' + i + '">'
+			+ '<div class="front"></div>'
+			+ '<div class="back">'
+			+ '<p class="cardname">' + card.name + '</p>'
+			+ '<img src="' + card.src + '">';
+		for (var n = 0; n < card.details.length; n++) {
+			html[i] += '<p>' + card.details[n] + '</p>';
 		}
-	},
+		html[i] += '</div></div>';
+	});
+	// duplicate each card
+	html = html.concat(html);
+	// sort randomly
+	html.sort(this.shuffle);
+	// render to html
+	this.cardsElement.innerHTML = html.join('');
 
-	cardClicked: function(event) {
-		var card = event.target.parentElement;
-		var game = this;
-
-		if (game.selected.length === 0) {
-			card.classList.toggle('flipped');
-			game.selected.push(card);
-			return;
-		}
-		if (game.selected.length === 1) {
-			card.classList.toggle('flipped');
-			game.selected.push(card);
-			game.setTry();
-
-			if (game.selected[0].dataset.index ===
-				card.dataset.index) {
-				//match
-				game.selected = [];
-				game.setMatch();
-			} else {
-				// no match
-				setTimeout(game.resetSelected.bind(game), 1000);
-			}
-		}
-		if (game.matches === game.cards.length) {
-			// game over
-			clearInterval(game.timer);
-			setTimeout(game.renderInfo.bind(game), 1000);
-		}
-	},
-
-	resetSelected: function() {
-		this.selected[0].classList.toggle('flipped');
-		this.selected[1].classList.toggle('flipped');
-		this.selected = [];
-	},
-
-	shuffle: function() {
-		return Math.random() - 0.5;
-	},
-
-	setTry: function() {
-		this.tries++;
-		if (this.tries === 1) {
-			this.timer = setInterval(this.renderTime.bind(this), 1000);
-		}
-		this.renderScore();
-	},
-
-	setMatch: function() {
-		this.matches++;
-		this.renderScore();
-	},
-
-	renderScore: function() {
-		this.scoreElement.innerText = this.tries + ' / ' + this.matches;
-	},
-
-	renderTime: function() {
-		this.timeElement.innerText = this.time++;
-	},
-
-	renderInfo: function() {
-		var msg = 'You solved the game in '
-			+ this.time + ' seconds with '
-			+ this.tries + ' tries.';
-
-		document.getElementById('infotext').innerText = msg;
-		this.infoElement.classList.toggle('hidden');
-
-		document.getElementById('btnYes').addEventListener('click', function() {
-			location.reload();
-		});
-		document.getElementById('btnNo').addEventListener('click',function() {
-			this.infoElement.classList.toggle('hidden');
-		}.bind(this));
+	// add event handler for each card
+	var elements = document.getElementsByClassName('front');
+	for(var i = 0; i < elements.length; i++) {
+		elements[i].addEventListener('click', this.cardClicked.bind(this));
 	}
-
 };
+
+/**
+ * Event handler for card click
+ */
+Match.prototype.cardClicked = function(event) {
+	var card = event.target.parentElement;
+	var game = this;
+
+	// first card selected, save to array
+	if (game.selected.length === 0) {
+		card.classList.toggle('flipped');
+		game.selected.push(card);
+		return;
+	}
+	// second card selected, save to array and check for match
+	if (game.selected.length === 1) {
+		card.classList.toggle('flipped');
+		game.selected.push(card);
+		game.setTry();
+
+		if (game.selected[0].dataset.index ===
+			card.dataset.index) {
+			//match
+			game.selected = [];
+			game.setMatch();
+		} else {
+			// no match
+			setTimeout(game.resetSelected.bind(game), 1000);
+		}
+	}
+	// check for all cards matched
+	if (game.matches === game.cards.length) {
+		// game over
+		clearInterval(game.timer);
+		setTimeout(game.renderInfo.bind(game), 1000);
+	}
+};
+
+/**
+ * Reset cards if no match
+ */
+Match.prototype.resetSelected = function() {
+	this.selected[0].classList.toggle('flipped');
+	this.selected[1].classList.toggle('flipped');
+	this.selected = [];
+};
+
+/**
+ * Return random value for sorting
+ */
+Match.prototype.shuffle = function() {
+	return Math.random() - 0.5;
+};
+
+/**
+ * Increment tries, start game timer after first try
+ */
+Match.prototype.setTry = function() {
+	this.tries++;
+	if (this.tries === 1) {
+		this.timer = setInterval(this.renderTime.bind(this), 1000);
+	}
+	this.renderScore();
+};
+
+/**
+ * Increment matches
+ */
+Match.prototype.setMatch = function() {
+	this.matches++;
+	this.renderScore();
+};
+
+/**
+ * Render tries and matches to html
+ */
+Match.prototype.renderScore = function() {
+	this.scoreElement.innerText = this.tries + ' / ' + this.matches;
+};
+
+/**
+ * Render game timer to html
+ */
+Match.prototype.renderTime = function() {
+	this.timeElement.innerText = this.time++;
+};
+
+/**
+ * Render game over screen
+ */
+Match.prototype.renderInfo = function() {
+	var msg = 'You solved the game in '
+		+ this.time + ' seconds with '
+		+ this.tries + ' tries.';
+
+	document.getElementById('infotext').innerText = msg;
+	this.infoElement.classList.toggle('hidden');
+
+	document.getElementById('btnYes').addEventListener('click', function() {
+		location.reload();
+	});
+	document.getElementById('btnNo').addEventListener('click',function() {
+		this.infoElement.classList.toggle('hidden');
+	}.bind(this));
+};
+
+global.Match = Match;
+
+})(this);
